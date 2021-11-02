a845cce07af206400b13d1f734eda663f2967457
Ioannis Kousis <bonobo@otenet.gr>
2021-11-02 10:13:26 +0200

    ultimate feature

---
+++ sample.rb
@@ -0,0 +1,27 @@
+require "active_support/core_ext/numeric/time"
+
+@sessionsByUser = {}
+
+def addEvent(event)
+
+end
+
+data = {
+  "events": [
+    
+  ]
+}
+
+1
+
+2
+
+3
+
+4
+
+-------------------------
+
+5
+
+6
